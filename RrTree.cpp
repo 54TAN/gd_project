e6a1fa361fdbdd667e4650f0e023a4d422607127
tdc2 <tantodelculo@yandex.ru>
2017-02-23 21:56:17 +0300

    list of  'x'

--- RrTree.cpp
+++ RrTree.cpp
@@ -7,7 +7,7 @@ RrTree::RrTree(Map* the_map, double distance, bool search) :
         goal_state(RrtNode(the_map->points.back())),
         min_distance(distance)
 {
-    nodes.push_back(RrtNode(the_map->points.front())); // начальная точка
+    nodes.push_back(RrtNode(the_map->points.front()));
     double temp[2] = {the_map->points.front().x, the_map->points.front().y};
     KdTree kd;
     kd.nodes.push_back(KdNode(temp, 0));
