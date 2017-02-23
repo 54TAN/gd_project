5e7285245803682a5c7842521a6d3706a072214d
tdc2 <tantodelculo@yandex.ru>
2017-02-23 21:36:34 +0300

    moving

--- main.cpp
+++ main.cpp
@@ -18,20 +18,15 @@ int main(int argc, char ** argv) {
     my_map.width = width;
     my_map.height = height;
 
-    //my_map.obstacles.push_back(Obstacle(Point(180, 0), Point(190, 300)));
     my_map.obstacles.push_back(Obstacle(Point(380, 200), Point(390, 500)));
     my_map.obstacles.push_back(Obstacle(Point(780, 200), Point(790, 500)));
-    //my_map.obstacles.push_back(Obstacle(Point(560, 330), Point(570, 500)));
-    //my_map.obstacles.push_back(Obstacle(Point(490, 250), Point(700, 260)));
-    //my_map.obstacles.push_back(Obstacle(Point(620, 430), Point(700, 440)));
-    //my_map.obstacles.push_back(Obstacle(Point(660, 460), Point(670, 500))); //финальная висяка*//*
     my_map.obstacles.push_back(Obstacle(Point(580, 0), Point(590, 300)));
     my_map.points.push_back(Point(2, 498));
     my_map.points.push_back(Point(998, 498));
 
     clock_t t = clock();
 
-    RrTree my_rrt(&my_map, 50, 1); // все финальные тесты на 50
+    RrTree my_rrt(&my_map, 50, 1);
     my_rrt.get_path(my_rrt.nodes.size() - 1);
 
     t = (clock() - t) / CLOCKS_PER_SEC;
